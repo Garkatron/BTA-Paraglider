f703cce3d7548728f5fe011fa4484619e9bab7ae
Garkatron <145294535+Garkatron@users.noreply.github.com>
2024-09-28 16:07:34 +0200

    1.0.1 * Added "random.click" when you use ParagliderItem.java

--- src/main/java/deus/paraglider/ParagliderItem.java
+++ src/main/java/deus/paraglider/ParagliderItem.java
@@ -4,22 +4,27 @@ package deus.paraglider;
 import deus.gravitylib.interfaces.IGravityObject;
 import deus.gravitylib.interfaces.accesor.ILivingEntity;
 import deus.gravitylib.main;
+import net.minecraft.client.Minecraft;
 import net.minecraft.core.entity.Entity;
 import net.minecraft.core.entity.player.EntityPlayer;
 import net.minecraft.core.item.Item;
 import net.minecraft.core.item.ItemStack;
+import net.minecraft.core.sound.SoundCategory;
 import net.minecraft.core.world.World;
 
 public class ParagliderItem extends Item {
 
 	protected boolean activated = false;
+	private Minecraft minecraft;
 
 	public ParagliderItem(int id) {
 		super(id);
+		minecraft = Minecraft.getMinecraft(this);
 	}
 
 	public ParagliderItem(String name, int id) {
 		super(name, id);
+		minecraft = Minecraft.getMinecraft(this);
 	}
 
 	@Override
@@ -28,6 +33,11 @@ public class ParagliderItem extends Item {
 		IGravityObject iLivingEntity = (IGravityObject) entityplayer;
 		iLivingEntity.gravityLib$setYGravityScale(0.1f);
 		activated = !activated;
+
+
+		this.minecraft.sndManager.playSound("random.click", SoundCategory.GUI_SOUNDS, 1.0F, 1.0F);
+
+
 		return super.onUseItem(itemstack, world, entityplayer);
 	}
 
